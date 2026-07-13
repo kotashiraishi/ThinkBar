bde996d931fe7038fda9f758f833f6efde3ed6bd
kotashiraishi <ricco.kuhn@gmail.com>
2026-07-13 15:34:18 +0900

    initial project

---
+++ ThinkBar/ThinkBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkBarApp.swift
+//  ThinkBar
+//
+//  Created by Kota Shiraishi on 2026/07/13.
+//
+
+import SwiftUI
+
+@main
+struct ThinkBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThinkBarTests/ThinkBarTests.swift
@@ -0,0 +1,18 @@
+//
+//  ThinkBarTests.swift
+//  ThinkBarTests
+//
+//  Created by Kota Shiraishi on 2026/07/13.
+//
+
+import Testing
+
+struct ThinkBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
